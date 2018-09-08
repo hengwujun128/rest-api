dd5fa8bc08c68788237d18411e75014b7d26257d
天山来客 <hengwujun128@gmail.com>
2018-09-09 01:37:40 +0800

    add rest-api

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js This file will be used for configuring the app,
+
+/**
+ * All of the logic will be put in its respective directory regarding the specific feature it will be implementing. We use module.exports to make this app object visible to the rest of the program when we call for it using require(
+ *
+ */
+
+var express = require('express');
+var app = express();
+module.exports = app;
